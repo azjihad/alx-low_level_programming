f18bf4d9c2da03ede245fe6f5182f12169e8003b
azjihad <jihad.azam.dev@gmail.com>
2023-06-21 12:53:56 -0700

    task 6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,7 +6,6 @@
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
- * done by : j.azam
  */
 int print_sign(int n)
 {
@@ -16,11 +15,4 @@ int print_sign(int n)
 		return (1);
 	} else if (n == 0)
 }
-		_putchar(48);
-		return (0);
-	} else if (n < 0)
-	{
-		_putchar('-');
-	}
-		return (-1);
-}
+
