8e75f92a65340220c292f796849222e55dbe9aaa
azjihad <jihad.azam.dev@gmail.com>
2023-06-23 14:50:05 -0700

    task 0

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -6,12 +6,12 @@
  */
 void positive_or_negative(int i)
 {
-    if (i > 0)
-        printf("%d is positive\n", i);
-    else if (i < 0)
-        printf("%d is negative\n", i);
-    else
-        printf("%d is zero\n", i);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
 }
 
 /**
@@ -20,10 +20,10 @@ void positive_or_negative(int i)
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    i = 0;
-    positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-    return (0);
+	return (0);
 }
