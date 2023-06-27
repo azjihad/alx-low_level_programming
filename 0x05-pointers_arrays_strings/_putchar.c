48258631a2fe035ecbdadc6d413f3c3f999d7467
azjihad <jihad.azam.dev@gmail.com>
2023-06-27 06:03:32 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - Writes a character to the standard output (stdout)
+ * @c: The character to be written
+ *
+ * Return: On success, returns the character written as an unsigned char
+ *         On error, returns -1
+ */
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
