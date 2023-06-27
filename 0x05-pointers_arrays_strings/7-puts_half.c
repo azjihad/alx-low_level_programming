70d5c1164dc2e6b458f02056285260b41dbb1dd2
azjihad <jihad.azam.dev@gmail.com>
2023-06-27 06:36:46 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,7 +16,7 @@ void puts_half(char *str)
 	if (length % 2 == 0)
 		half_length = length / 2;
 	else
-		half_length = (length - 1) / 2;
+		half_length = (length + 1) / 2;
 
 	for (i = half_length; i < length; i++)
 		_putchar(str[i]);
